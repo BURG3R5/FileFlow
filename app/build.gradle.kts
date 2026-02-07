6c48bf739e0a8c5125886ca19afb5011d49bd949
Aditya Rajput <adiraj20072002@gmail.com>
2026-02-08 03:14:52 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "co.adityarajput.fileflow"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
